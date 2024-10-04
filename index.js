a1d2d973c01bb416ec7bee36dc2e06c646737e9e
aujde <134573316+aujde@users.noreply.github.com>
2024-10-04 11:51:00 +1000

    mongodb and dotenv

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+require('dotenv').config();
+
 const express = require('express');
 const session = require('express-session');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 app.set('view engine', 'ejs');
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-    secret: 'mysecret',
+    secret: process.env.SESSION_SECRET || 'defaultsecret',
     resave: false,
     saveUninitialized: false
 }));
 
 const PORT = process.env.PORT || 3000;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_SERVER}`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
@@ -21,3 +31,19 @@ app.listen(PORT, () => {
 app.get('/status', (req, res) => {
     res.json({ status: 'ok' });
 });
+
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
