fd1823405f6ebbd743780eca0369335d4d624640
aujde <134573316+aujde@users.noreply.github.com>
2024-10-06 14:58:55 +1100

    merge and db singleton?

--- app/utils/db.js
+++ app/utils/db.js
@@ -17,4 +17,41 @@ async function connectToMongo() {
     return client;
 }
 
-module.exports = { connectToMongo };
+class MongoDB {
+    constructor() {
+        if (!MongoDB.instance) {
+            MongoDB.instance = this;
+        }
+        return MongoDB.instance;
+    }
+
+    async getCollection(collectionName) {
+        const client = await connectToMongo();
+        return client.db().collection(collectionName);
+    }
+
+    async find(collectionName, query) {
+        const collection = await this.getCollection(collectionName);
+        return collection.find(query).toArray();
+    }
+
+    async insertOne(collectionName, document) {
+        const collection = await this.getCollection(collectionName);
+        return collection.insertOne(document);
+    }
+
+    async updateOne(collectionName, filter, update) {
+        const collection = await this.getCollection(collectionName);
+        return collection.updateOne(filter, update);
+    }
+
+    async deleteOne(collectionName, filter) {
+        const collection = await this.getCollection(collectionName);
+        return collection.deleteOne(filter);
+    }
+}
+
+const instance = new MongoDB();
+Object.freeze(instance);
+
+module.exports = instance;
